3e168756d8b3d56efd54a7d7c1486f30e34e1ff5
Nguyen Huy Hoang <huyhoang681997@gmail.com>
2018-09-21 18:58:22 +0700

    Hoang finish 'Find Integer By Recursive' project

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Find the number in the fibonacci range");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an index in the fibonacci range:");
+
+        int index = scanner.nextInt();
+
+        int result = findValue(index);
+
+        System.out.println("The value at position " + index + " in the fibonacci range is: " + result);
+    }
+
+    public static int findValue(int index) {
+
+        if (index < 3) return 1;
+
+        return findValue(index - 2) + findValue(index - 1);
+
+    }
+}
